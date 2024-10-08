de46ee510fa9e4d93ab0ad0875675188971f41d7
PavelCernii <pavel.cernii@mail.ru>
2024-10-08 11:59:30 +0200

    11:59

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,8 @@
+
+name = "Adam"
+age = 20  
+height = 175 
+
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age + 6} years old.")
