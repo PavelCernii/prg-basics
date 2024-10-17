f8b121998e320e88fe64df4da757ba6a95a25e63
PavelCernii <236159@student.uek.krakow.pl>
2024-10-17 21:57:46 +0200

    21:57

--- 03-ControlStructures/1-7.py
+++ 03-ControlStructures/1-7.py
@@ -4,14 +4,14 @@
 #
 basic_salary = 5000
 total_salary = 0
-is_bonus = True # does the employee receive a bonus
-bonus = 0.15 # 15%
+is_bonus = int(input('Does the employee has bonus? (1 - Yes,2 - No): ')) # does the employee receive a bonus
+bonus = 0.3 # 15%
 
-if is_bonus == True:
-    total_salary = basic_salary + basic_salary*bonus
+if is_bonus == 1:
+    total_salary = basic_salary + basic_salary * bonus
 else:
     total_salary = basic_salary
 
 print(f'Basic salary: {basic_salary}')
-print(f'Bonus included? {is_bonus}')
+print(f'Bonus included? {"Yes" if is_bonus == 1 else "No"}')
 print(f'Total salary: {total_salary}')
---
+++ 03-ControlStructures/2-6.py
@@ -0,0 +1,8 @@
+number = int(input('Enter nuber: '))
+
+if number == 0:
+    print('Number is 0')
+elif number > 0:
+    print(f'Number {number} is positive')
+else: 
+    print(f'Number {number} is negative') 
